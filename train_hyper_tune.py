9a41dce2577a7bc6beab5a776beb81ffa070d4d3
Ze Yang <yangze0930@outlook.com>
2022-03-27 20:52:57 -0400

    fix minor bug

--- train_hyper_tune.py
+++ train_hyper_tune.py
@@ -6,7 +6,7 @@ import os
 from ray import tune
 from ray.tune import CLIReporter
 from ray.tune.schedulers import ASHAScheduler
-from parse_config import _update_config
+from parse_config import ConfigParser
 from functools import partial
 from train import main
 
@@ -18,6 +18,7 @@ torch.backends.cudnn.benchmark = False
 np.random.seed(SEED)
 
 def hyper_tune(train_config, tune_config, num_samples=10, max_num_epochs=10, gpus_per_trial=1):
+    work_dir = os.getcwd()
     scheduler = ASHAScheduler(
         metric="val_loss",
         mode="min",
@@ -27,24 +28,26 @@ def hyper_tune(train_config, tune_config, num_samples=10, max_num_epochs=10, gpu
     reporter = CLIReporter(
         metric_columns=["loss", "accuracy", 'val_loss', 'val_accuracy', "training_iteration"])
     result = tune.run(
-        partial(single_job, train_config=train_config, work_dir=os.getcwd()),
+        partial(single_job, train_config=train_config, ori_work_dir=work_dir),
         resources_per_trial={"cpu": 2, "gpu": gpus_per_trial},
         config=tune_config,
         num_samples=num_samples,
         scheduler=scheduler,
-        progress_reporter=reporter)
+        progress_reporter=reporter,
+        local_dir=os.path.join(work_dir, "hyper_results"))
     best_trial = result.get_best_trial("val_loss", "min", "last")
     print("Best trial config: {}".format(best_trial.config))
     print("Best trial final validation loss: {}".format(
-        best_trial.last_result["loss"]))
+        best_trial.last_result["val_loss"]))
     print("Best trial final validation accuracy: {}".format(
-        best_trial.last_result["accuracy"]))
+        best_trial.last_result["val_accuracy"]))
 
 
-def single_job(tune_config, train_config, work_dir=None):
-    if work_dir is not None:
-        os.chdir(work_dir)
-    train_config._config = _update_config(train_config._config, tune_config)
+def single_job(tune_config, train_config, ori_work_dir=None):
+    train_config._config["trainer"]["save_dir"] = os.getcwd()
+    if ori_work_dir is not None:
+        os.chdir(ori_work_dir)
+    train_config = ConfigParser(train_config._config, modification=tune_config)
     main(train_config, hyper_tune=True)
 
 
